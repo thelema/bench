70fb5562f0b48b784453ee557d2aa716cc2dc638
Eric Norige <thelema314@gmail.com>
2012-01-20 11:29:46 -0500

    benchmark from mailing list

--- examples/listmap.ml
+++ examples/listmap.ml
@@ -7,5 +7,5 @@ let () =
      "stdlib map", List.map ident;
      "rev rev_map", (fun l -> List.rev (List.rev_map ident l))]
     ~input_gen:(fun n -> BatList.init n ident)
-    ~n:30 (1,10_000)
-  |> Bench.print_2d "lm.out"
+    ~n:20 (1000,100_000)
+  |> Bench.print_2d "listmap.out"
